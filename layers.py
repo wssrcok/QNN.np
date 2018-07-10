fixed quantize gradients
..

layers.py
@@ -1,5 +1,5 @@
 import numpy as np
-from quantize import truncate_features, truncate_weights
+from quantize import truncate_unsigned, truncate_signed
 from im2col import *
 
 def conv2d(weight_id = -1, hparameters = {'stride': 1, 'pad': 2}, truncate = 0):
@@ -28,7 +28,7 @@ def conv2d(weight_id = -1, hparameters = {'stride': 1, 'pad': 2}, truncate = 0):
         return out, cache
     return layer
 
-def conv2d_b(grad_id = -1):
+def Quantized_conv2d_b(grad_id = -1, truncate = False):
     '''
     Arguments:
     dZ -- numpy array of shape (m, n_C, n_H, n_W) 
@@ -62,6 +62,8 @@ def conv2d_b(grad_id = -1):
         dA_prev = col2im_indices(dA_prev_col, A_prev.shape, f, f, padding=pad, stride=stride)
         grads["dW"+str(grad_id)] = dW.astype(np.float32)
         grads["db"+str(grad_id)] = db.astype(np.float32)
+        if truncate:
+            dA_prev = truncate_signed(dA_prev, truncate)
         return dA_prev
     return layer
     
@@ -79,7 +81,7 @@ def Quantized_ReLu(truncate = False):
         out = np.maximum(0,x)
         cache = x
         if truncate:
-            out = truncate_features(x, truncate)
+            out = truncate_unsigned(x, truncate)
         return out, cache
     return layer
 
@@ -125,7 +127,6 @@ def softmax_b():
 def max_pool(hparameters = {'f': 2, 'stride': 2}):
     def layer(A_prev, dummy_parameters):
         print('max_pool',  end=' => ', flush=True)
-        print('\nsample feature: ', A_prev[0,0,0:5,0:5])
         # Let say our input X is 5x10x28x28
         # Our pooling parameter are: size = 2x2, stride = 2, padding = 0
         # i.e. result of 10 filters of 3x3 applied to 5 imgs of 28x28 with stride = 1 and padding = 1
@@ -204,7 +205,7 @@ def unflatten():
         return out
     return layer
 
-def dense(weight_id = -1, truncate = 0):
+def dense(weight_id = -1):
     """
     Implement the linear part of a layer's forward propagation.
 
@@ -227,7 +228,7 @@ def dense(weight_id = -1, truncate = 0):
         return Z, cache
     return layer
 
-def dense_b(grad_id = -1):
+def Quantized_dense_b(grad_id = -1, truncate = False):
     def layer(dZ, cache, grads):
         print('dense_b',  end=' => ', flush=True)
         """
@@ -254,6 +255,8 @@ def dense_b(grad_id = -1):
         assert (db.shape == b.shape)
         grads["dW"+str(grad_id)] = dW.astype(np.float32)
         grads["db"+str(grad_id)] = db.astype(np.float32)
+        if truncate:
+            dA_prev = truncate_signed(dA_prev, truncate)
         return dA_prev
     return layer
 
@@ -294,7 +297,7 @@ def initialize_weights_random_normal(filter_dims, layer_dims, truncate = 0, seed
         parameters['W' + str(l+1)] = np.random.randn(n_C, n_C_prev, f, f).astype(np.float32) * np.float32(0.25)
         parameters['b' + str(l+1)] = np.zeros((n_C,1)).astype(np.float32)
         if truncate:
-            parameters["W" + str(l+1)] = truncate_weights(parameters["W" + str(l+1)], truncate)
+            parameters["W" + str(l+1)] = truncate_signed(parameters["W" + str(l+1)], truncate)
         l+=1
     
     for l in range(1, L2):
@@ -302,8 +305,8 @@ def initialize_weights_random_normal(filter_dims, layer_dims, truncate = 0, seed
         parameters['W' + str(l+L)] = np.random.randn(layer_dims[l],layer_dims[l-1]).astype(np.float32)*np.float32(0.01)
         parameters['b' + str(l+L)] = np.zeros((layer_dims[l],1)).astype(np.float32)
         if truncate:
-            parameters["W" + str(l+L)] = truncate_weights(parameters["W" + str(l+L)], truncate, weights_range = 0.08)
-            parameters["b" + str(l+L)] = truncate_weights(parameters["b" + str(l+L)], truncate, weights_range = 0.08)
+            parameters["W" + str(l+L)] = truncate_signed(parameters["W" + str(l+L)], truncate, weights_range = 0.08)
+            parameters["b" + str(l+L)] = truncate_signed(parameters["b" + str(l+L)], truncate, weights_range = 0.08)
         l+=1
     return parameters
 
@@ -315,7 +318,6 @@ def update_weights(parameters, grads, learning_rate, truncate = 0):
         parameters["W" + str(l+1)] -= np.float32(learning_rate * grads['dW'+str(l+1)])
         parameters["b" + str(l+1)] -= np.float32(learning_rate * grads['db'+str(l+1)])
         if truncate:
-            parameters["W" + str(l+1)] = truncate_weights(parameters["W" + str(l+1)], truncate)
-            parameters["b" + str(l+1)] = truncate_weights(parameters["b" + str(l+1)], truncate)
-    print('sample weights: ', parameters['W1'][0,0,0:5,0:5])
+            parameters["W" + str(l+1)] = truncate_signed(parameters["W" + str(l+1)], truncate)
+            parameters["b" + str(l+1)] = truncate_signed(parameters["b" + str(l+1)], truncate)
     return parameters

main.py
@@ -36,7 +36,8 @@ def predict(model, eval_set, weights):
             
 
 def train(models, layer_dims, train_set, 
-          truncate = False, 
+          truncate_f = False,
+          truncate_b = False, 
           learning_rate = 0.02, 
           decay_rate = 2,
           batch_size = 32, 
@@ -69,15 +70,15 @@ def train(models, layer_dims, train_set,
             # model goes entire forward pass
             y_hat, caches = model(x, weights)
             caches[-1] = y # y is cached for softmax_b
-            cost = reduce_mean_softmax_cross_entropy_loss(y_hat, y, truncate = truncate)
+            cost = reduce_mean_softmax_cross_entropy_loss(y_hat, y, truncate = truncate_f)
             # model_b goes entire backward pass
             model_b(y_hat, caches, grads) # this will also update all gradients
 
-            truncate_grads(grads) # TESTCODE
+            truncate_grads(grads, truncate_b) # TESTCODE
 
             # update weights using minibatch gradient decent
             learning_rate = 1 / (1 + decay_rate * i) * learning_rate_o
-            update_weights(weights, grads, np.float32(learning_rate), truncate = truncate)
+            update_weights(weights, grads, np.float32(learning_rate), truncate = truncate_f)
 
             # print cost
             print ("\nCost after Epoch %i, batch %i: %f \n" %(i+1, j+1, cost))
@@ -93,10 +94,12 @@ def train(models, layer_dims, train_set,
 
 # TODO: add quantization
 def main():
-    if len(sys.argv) != 5 and len(sys.argv) != 4 and len(sys.argv) != 1:
+    args = len(sys.argv)
+    if args == 0 or args == 2 or args == 3:
         print('usage1: $ python main.py')
         print('usage2: $ python main.py <batch_size(int)> <learning_rate(float)> <num_epochs(int)>')
         print('usage3: $ python main.py <batch_size(int)> <learning_rate(float)> <num_epochs(int)> <quantize_bits(int)>')
+        print('usage3: $ python main.py <batch_size(int)> <learning_rate(float)> <num_epochs(int)> <quantize_bits(int)> <quantize_grads(int)>')
         return None
 
     train_data, train_labels, eval_data, eval_labels, classes = load_dataset()
@@ -106,22 +109,29 @@ def main():
     dense_dims = [3136, 1024, classes]
     layer_dims = (conv_dims, dense_dims)
 
-    models = (MNIST_model(), MNIST_model_b()) 
     weights = {}
-    if len(sys.argv) == 1:
-        weights = train(models, layer_dims, train_set)
-    elif len(sys.argv) == 4:
-        weights = train(models, layer_dims, train_set,
-            batch_size = int(sys.argv[1]),
-            learning_rate = float(sys.argv[2]),
-            num_epochs = int(sys.argv[3]))
-    else: 
-        models = (MNIST_model(int(sys.argv[4])), MNIST_model_b())
-        weights = train(models, layer_dims, train_set,
-            batch_size = int(sys.argv[1]),
-            learning_rate = float(sys.argv[2]),
-            num_epochs = int(sys.argv[3]),
-            truncate = int(sys.argv[4]))
+    batch_size = 32
+    learning_rate = 0.02
+    num_epochs = 10
+    truncate_f = False
+    truncate_b = False
+    if args >= 4:
+        batch_size = int(sys.argv[1])
+        learning_rate = float(sys.argv[2])
+        num_epochs = int(sys.argv[3])
+    if args >= 5:
+        truncate_f = int(sys.argv[4])
+    if args == 6:
+        truncate_b = int(sys.argv[5])
+        
+    models = (MNIST_model(truncate_f), MNIST_model_b(truncate_b))
+    print(truncate_b, truncate_f)
+    weights = train(models, layer_dims, train_set,
+                    batch_size=batch_size,
+                    learning_rate=learning_rate,
+                    num_epochs=num_epochs,
+                    truncate_f=truncate_f,
+                    truncate_b=truncate_b)
 
     eval_set = (eval_data, eval_labels)
     predict(MNIST_model(), eval_set, weights)

quantize.py
@@ -35,7 +35,7 @@ def test_ub_rounding_negative(num = -0.1, k = 1000):
 
 
 
-def truncate_weights(w, bits):
+def truncate_signed(w, bits):
     '''
     Argument:
     w -- np array of weights
@@ -51,7 +51,7 @@ def truncate_weights(w, bits):
     return q.astype(np.float32)
     # return w
 
-def truncate_features(f, bits):
+def truncate_unsigned(f, bits):
     '''
     Argument:
     f -- np array of features
@@ -67,7 +67,7 @@ def truncate_features(f, bits):
     return q.astype(np.float32)
     # return f
 
-def truncate_grads(grads):
-    for k,v in grads.items():
-        grads[k] = truncate_weights(v, 2)
-    print('sample grads:' + str(grads['dW1'][0,0]))
+def truncate_grads(grads, truncate):
+    if truncate:
+        for k,v in grads.items():
+            grads[k] = truncate_signed(v, truncate)